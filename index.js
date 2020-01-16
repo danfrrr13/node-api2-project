3a5a2a1d46b34da07a4b208bed7acb73730b9877
Daniel Ferrer <danfrrr13@gmail.com>
2020-01-16 08:25:08 -0500

    MVP

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const postsRouter = require('./posts/postsRouter.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', postsRouter);
+
+server.listen(4000, () => {
+    console.log('\n*** Server Running on http://localhost:4000 ***\n');
+});
